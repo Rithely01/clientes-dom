3738632dfd47dda2c44c773fec87b2e5aac455c0
unknown <rithelynunes6213@digitalcollege.internal>
2025-07-08 11:54:21 -0300

    ta ok

--- script.js
+++ script.js
@@ -18,7 +18,55 @@ const btnCancelarEdicao = document.getElementById('cancelar-edicao');
 const btnConfirmarExclusao = document.getElementById('confirmar-exclusao');
 const btnCancelarExclusao = document.getElementById9('cancelar-exclusao');
 
-let clientes = [];
+let clientes = [
+    {
+        nome: 'Samuel',
+        sobrenome:'dos Santos',
+        cpf:'71233879377',
+        email:'samuel28@gmail.com',
+    },
+
+      {
+        nome: 'Myrela',
+        sobrenome:'Ferreira',
+        cpf:'71233879377',
+        email:'Myrela08@gmail.com',
+    },
+        
+    
+      {
+        nome: 'Muceguinha',
+        sobrenome:'Costa',
+        cpf:'71233879377',
+        email:'MuceguinhaLinda07@gmail.com',
+    },
+        
+];
 
 let indexEditando = null;
 let indexExcluindo = null;
+
+function renderizarTabela() {
+    clientes.forEach((cliente) => {
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = ´
+        <td>${cliente.nome}</td>
+        <td>${cliente.sobrenome}</td>
+        <td>${cliente.cpf}</td>
+        <td>${cliente.email}</td>
+        ´;
+
+        const tdAcoes = document.createElement('td');
+        tdAcoes.classList.add('acoes');
+        btnEditar.textContent = 'Editar';
+        btnEditar.classList.add('editar');
+
+        const btnExcluir = document.createElement('button');
+        btnExcluir.textContent = 'Excluir';
+        btnExcluir.classList.add('excluir');
+
+        tdAcoes.appendChild()
+
+
+    })
